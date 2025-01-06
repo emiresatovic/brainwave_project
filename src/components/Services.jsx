6846fa8331861ea6e5b1fab9633479b542cadc1a
Emir Esatović <165719085+emiresatovic@users.noreply.github.com>
2025-01-06 16:31:35 +0100

    06.01 16h

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,4 +1,4 @@
-import { check, service1, service2 } from "../assets"
+import { check, service1, service2, service3 } from "../assets"
 import { brainwaveServices, brainwaveServicesIcons } from "../constants"
 import Heading from "./Heading"
 import Section from "./Section"
@@ -56,15 +56,22 @@ const Services = () => {
                        {brainwaveServicesIcons.map((item, index) => (
                         <li key={index} className={`rounded-2xl flex items-center justify-center ${index === 2 ? 'w-[3rem] h-[3rem] p-0.25 bg-conic-gradient md:w-[4.5rem] md:h-[4.5rem]' : 'flex w-10 h-10 bg-n-6 md:w-15 md:h-15'}`}>
                             <div className={index === 2 ? 'flex items-center justify-center w-full h-full bg-n-7 rounded-[1rem]' : ''}>
-                                <img src={item} alt="" />
+                                <img src={item} width={24} height={24} alt={item} />
                             </div>
                         </li>
                        ))}
                     </ul>
                   </div>
-               </div>
 
+                  <div className="relative h-[20rem] bg-n-8 rounded-xl overflow-hidden md:h-[25rem]">
+                    <img src={service3} className="w-full h-full object-cover" width={520} height={500} alt="sacry robot" />
+                    <VideoChatMessage />
+                    <VideoBar />
+                  </div>
+               </div>
             </div>
+
+            <Gradient />
            </div>
         </div>
     </Section>
